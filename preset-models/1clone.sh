ed641a2cfffec2015324f784884aca086fbb0e4b
masfrank <stinowork@outlook.it>
2022-10-20 02:23:58 +0800

    rm smartdns angon change to material

--- preset-models/1clone.sh
+++ preset-models/1clone.sh
@@ -7,15 +7,15 @@ mv ./lede/* ./
 # 删除原默认主题
 rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-theme-bootstrap
-rm -rf package/lean/luci-theme-material
+#rm -rf package/lean/luci-theme-material
 rm -rf package/lean/luci-theme-netgear
 rm -rf package/lean/luci-theme-ifit
 rm -rf package/lean/luci-theme-neobird
 
 mkdir -p package/myapps
 cd package/myapps
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-git clone https://github.com/jerrykuku/luci-app-argon-config.git
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+#git clone https://github.com/jerrykuku/luci-app-argon-config.git
 git clone https://github.com/kenzok8/openwrt-packages.git
 git clone https://github.com/Ausaci/luci-app-nat6-helper.git
 git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
