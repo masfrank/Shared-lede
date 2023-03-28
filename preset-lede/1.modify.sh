c1fedd7a470739272d132341f41ef3b395d408b1
masfrank <stinowork@outlook.it>
2023-03-28 19:46:11 +0800

    vim

--- preset-lede/1.modify.sh
+++ preset-lede/1.modify.sh
@@ -33,12 +33,6 @@ cp extra-files/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patch
 #设置WIFI
 sed -i 's/OpenWrt/Xiaomi_E2B0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-#取消原主题luci-theme-bootstrap为默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
-# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
 # 修改默认wifi密码key为password
 sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
